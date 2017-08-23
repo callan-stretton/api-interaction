f6cde074aed7cbaa66e1a011f3148bcb8bd05686
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-08-23 23:35:28 +1200

    added tweet function

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-var proverbP = document.getElementById("proverb");
-var translationP = document.getElementById("translation");
+var proverbP = document.getElementById("proverb"); // proverb paragrpah id
+var translationP = document.getElementById("translation"); // translation paragrpah id
 
-function getProverb (evt) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://eda-te-reo.herokuapp.com/api/proverbs');
+function getProverb (evt) { // runs when button clicked
+  var xhr = new XMLHttpRequest(); // defining my XML HTTP Request variable
+  xhr.open('GET', 'https://eda-te-reo.herokuapp.com/api/proverbs'); // initialising my request
   xhr.onload = function () {
-    var ranProverb = JSON.parse(xhr.responseText);
-    console.log(ranProverb.source);
-    console.log(ranProverb.translation);
-    proverbP.innerHTML = 'Proverb: ' + ranProverb.source;
-    translationP.innerHTML = 'Meaning: ' + ranProverb.translation;
+    var ranProverb = JSON.parse(xhr.responseText); // brings in data as JS object
+    proverbP.innerHTML = ranProverb.source; // only selecting the source(proverb) property
+    translationP.innerHTML = ranProverb.translation; // only selecting the translation(meaning) property
   };
-  xhr.send();
+  xhr.send(); // sends out XML HTTP Request
 };
 
-function addProverb(){
-
+function sendTweet() { // runs when button clicked
+  var url = "https://twitter.com/intent/tweet";
+  var text = document.getElementById("proverb").innerHTML;
+  var hashtags = "KaPai";
+  var via = "username";
+  window.open(url + "?text=" + text + ";hashtags=" + hashtags, "", "width=500, height=300"); // opens new window with text ready to send via Twitter
 }
